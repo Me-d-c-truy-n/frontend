dc2b87b4b25bc214533b4afddae0dc0c4954e1bf
21120447 <haonhat2729@gmail.com>
2024-05-12 16:09:56 +0700

    dynamic slider

--- frontend/src/components/Slider.tsx
+++ frontend/src/components/Slider.tsx
@@ -1,6 +1,12 @@
-import slider from '../assets/images/slider/home.webp';
+import slider_const from "../constants/slider.json"
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
 
 const Slider = () => {
+  const slider = slider_const["slider"][getRandomInt(slider_const["slider"].length)];
+
   return (
     <div className='w-full mb-5'>
         <img src={slider} alt="slider" className='w-full cursor-pointer'/>
