3e62d4b19fadfe2f0159488fdb7edb0a13d29af0
21120455 <hoai0455fit@gmail.com>
2024-05-13 10:21:45 +0700

    gell all novels

---
+++ back-end/src/main/java/com/crawldata/back_end/dto/Novel.java
@@ -0,0 +1,10 @@
+package com.crawldata.back_end.dto;
+
+public class Novel {
+    String novelId;
+    String novelName;
+    String image;
+    String description;
+    Integer total;
+    Author author;
+}
---
+++ back-end/src/main/java/com/crawldata/back_end/dto/NovelDetail.java
@@ -0,0 +1,4 @@
+package com.crawldata.back_end.dto;
+
+public class NovelDetail {
+}
