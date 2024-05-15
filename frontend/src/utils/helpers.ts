9f99934e89cde63053b1ee53d780036c039d445a
21120447 <haonhat2729@gmail.com>
2024-05-15 21:39:14 +0700

    add moment config time to now

--- frontend/src/utils/helpers.ts
+++ frontend/src/utils/helpers.ts
@@ -1,4 +1,15 @@
+import moment from 'moment'
+
 export function convertDate(date: string) {
-  const convertDate = new Date(date).toLocaleDateString("vi-VN");
-  return convertDate;
+  const convertDate = new Date(date);
+  return moment(convertDate).fromNow()
+        .replace('a few seconds', 'vài giây trước')
+        .replace('minute ago', 'phút trước')
+        .replace('minutes ago', 'phút trước')
+        .replace('hour ago', 'giờ trước')
+        .replace('hours ago', 'giờ trước')
+        .replace('day ago', 'ngày trước')
+        .replace('days ago', 'ngày trước')
+        .replace('an', '1')
+        .replace('a ', '1 ');  
 }
