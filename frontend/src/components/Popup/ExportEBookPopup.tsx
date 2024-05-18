7a12e5ff820bb362bcb27c27c6fdc29c310f850e
21120447 <haonhat2729@gmail.com>
2024-05-18 18:41:48 +0700

    scale when select export type

--- frontend/src/components/Popup/ExportEBookPopup.tsx
+++ frontend/src/components/Popup/ExportEBookPopup.tsx
@@ -40,7 +40,7 @@ const ExportEBookPopup = ({ close }: Props) => {
                 <button 
                   key={ep.id} 
                   onClick={() => setSelectedExport(ep.id)}
-                  className={`border border-slate-500 p-3 rounded-lg flex flex-col justify-center items-center uppercase ${selectedExport===ep.id?'shadow-lg bg-slate-900 text-white dark:bg-white dark:text-black ':'dark:bg-slate-950 bg-slate-100'}`} 
+                  className={`transition ease-in-out duration-300 border border-slate-500 p-3 rounded-lg flex flex-col justify-center items-center uppercase ${selectedExport===ep.id?'shadow-lg bg-slate-900 text-white dark:bg-white dark:text-black scale-110':'dark:bg-slate-950 bg-slate-100'}`} 
                 >
                   <img src={ep.image} alt={ep.id} className='w-14 h-14 mb-1'/>
                   {ep.id}
