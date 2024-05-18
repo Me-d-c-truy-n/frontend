673bab785f7aafdacdcb8ae9b3fd93863bd04443
21120447 <haonhat2729@gmail.com>
2024-05-18 11:02:22 +0700

    clean file

--- frontend/src/store/server/index.ts
+++ frontend/src/store/server/index.ts
@@ -2,10 +2,11 @@ import { createSlice } from "@reduxjs/toolkit";
 import { Store } from "../name";
 import { IServer } from "./type";
 import { serverReducer } from "./reducer";
+import { INITIAL_KEY } from "../../types/key";
 
 const initialState: IServer = {
   listServer: ["truyenfull", "tangthuvien"],
-  server: "truyenfull",
+  server: INITIAL_KEY.SERVER,
 }
 
 const serverSlice = createSlice({
