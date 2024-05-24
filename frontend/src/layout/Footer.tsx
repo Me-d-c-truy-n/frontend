8b84c6f39ccd076ed14104ad3012b88a1a2615d6
21120447 <haonhat2729@gmail.com>
2024-05-24 13:07:35 +0700

    feat: 404 typewrite

--- frontend/src/layout/Footer.tsx
+++ frontend/src/layout/Footer.tsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
 import logo from '../assets/images/logo.png';
 
+const APP_NAME = 
+  <span className="capitalize">{import.meta.env.VITE_REACT_APP_NAME}</span>;
+
 const Footer = () => {
   return (
     <div className="border-t-2 mt-5 p-5 pb-4">
       <div className="text-lg text-center text-gray-600">
-        Mê Truyện Chữ là nền tảng mở trực tuyến, miễn phí đọc truyện chữ được đóng góp nội dung từ các tác giả viết truyện và các dịch giả convert, dịch truyện, rất nhiều truyện hay và nổi bật được cập nhật nhanh nhất với đủ các thể loại tiên hiệp, kiếm hiệp, huyền ảo ...
+        {APP_NAME} là nền tảng mở trực tuyến, miễn phí đọc truyện chữ được đóng góp nội dung từ các tác giả viết truyện và các dịch giả convert, dịch truyện, rất nhiều truyện hay và nổi bật được cập nhật nhanh nhất với đủ các thể loại tiên hiệp, kiếm hiệp, huyền ảo ...
       </div>
       <div className='flex justify-center mt-2'>
         <img src={logo} alt='logo' width={90}/>
--- frontend/src/pages/error/Notfound.tsx
+++ frontend/src/pages/error/Notfound.tsx
@@ -1,8 +1,28 @@
 import { Link } from "react-router-dom"
 import { AiOutlineHome } from "react-icons/ai";
 import { Vortex } from "../../components/ui/vortex";
+import { TypewriterEffect } from "../../components/ui/typewriter-effect";
 
 const Notfound = () => {
+  const words = [
+    {
+      text: "404",
+      className: "text-sky-400",
+    },
+    {
+      text: "|",
+      className: "text-white",
+    },
+    {
+      text: "NOT",
+      className: "text-white",
+    },
+    {
+      text: "FOUND",
+      className: "text-white",
+    },
+  ];
+
   return (
     <div className="w-full mx-auto rounded-md  h-screen overflow-hidden">
     <Vortex
@@ -12,7 +32,7 @@ const Notfound = () => {
       baseHue={120}
       className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full"
     >
-      <p className="text-4xl text-white">404 | NOT FOUND</p>
+      <TypewriterEffect words={words}/>
       <Link to={'/'} replace className="flex gap-2 items-center text-slate-200 mt-10 text-lg hover:text-slate-100 border px-4 py-2 rounded-3xl hover:bg-sky-500 shadow-4xl hover:border-sky-500">
         <AiOutlineHome />
         Về trang chủ
