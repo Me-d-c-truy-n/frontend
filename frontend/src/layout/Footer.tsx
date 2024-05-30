643b445a2d5387c1682bb4188b207b7eb6a6fb76
21120447 <haonhat2729@gmail.com>
2024-05-30 12:48:14 +0700

    feat: footer app tooltip

--- frontend/src/layout/Footer.tsx
+++ frontend/src/layout/Footer.tsx
@@ -1,8 +1,46 @@
 import { Link } from 'react-router-dom';
-import logo from '../assets/images/logo.png';
+import { AnimatedTooltip } from '../components/ui/animated-tooltip';
+import admin_01 from "../assets/images/admin/admin_01.jpg";
+import admin_02 from "../assets/images/admin/admin_02.jpg";
+import admin_03 from "../assets/images/admin/admin_03.jpg";
+import admin_04 from "../assets/images/admin/admin_04.jpg";
+import logo from "../assets/images/logo.png";
 
 const APP_NAME = 
   <span className="capitalize">{import.meta.env.VITE_REACT_APP_NAME}</span>;
+
+  const people = [
+    {
+      id: 1,
+      name: import.meta.env.VITE_REACT_APP_NAME,
+      designation: "Group 04",
+      image: logo,
+    },
+    {
+      id: 2,
+      name: "Nguyễn Nhật Hào",
+      designation: "Frontend Developer",
+      image: admin_01,
+    },
+    {
+      id: 3,
+      name: "Tô Phương Hiếu",
+      designation: "Backend Developer",
+      image: admin_02,
+    },
+    {
+      id: 4,
+      name: "Trương Văn Hoài",
+      designation: "Backend Developer",
+      image: admin_03,
+    },
+    {
+      id: 5,
+      name: "Lê Minh Hoàng",
+      designation: "Backend Developer",
+      image: admin_04,
+    },
+  ];
 
 const Footer = () => {
   return (
@@ -10,10 +48,10 @@ const Footer = () => {
       <div className="text-lg text-center text-gray-600">
         {APP_NAME} là nền tảng mở trực tuyến, miễn phí đọc truyện chữ được đóng góp nội dung từ các tác giả viết truyện và các dịch giả convert, dịch truyện, rất nhiều truyện hay và nổi bật được cập nhật nhanh nhất với đủ các thể loại tiên hiệp, kiếm hiệp, huyền ảo ...
       </div>
-      <div className='flex justify-center mt-2'>
-        <img src={logo} alt='logo' width={90}/>
+      <div className="flex flex-row items-center justify-center mt-4 mb-2 w-full">
+        <AnimatedTooltip items={people} />
       </div>
-      <div className='mb-3 text-center font-bold dark:text-white'>FIT@HCMUS - 2024 - <Link to='/thong-tin/nhom-phat-trien'>Bee Group</Link></div>
+      <div className='-mr-4 mb-3 text-center font-bold dark:text-white'>FIT@HCMUS - 2024 - <Link to='/thong-tin/nhom-phat-trien'>Group 04</Link></div>
       <div className='flex justify-center mt-5 gap-6 text-gray-600'>
         <Link to='/thong-tin/nhom-phat-trien'>Nhóm phát triển</Link>
         <Link to='/thong-tin/dieu-khoan-dich-vu'>Điều khoản dịch vụ</Link>
