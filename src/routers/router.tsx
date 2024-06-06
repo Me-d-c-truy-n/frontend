5d99562df616e3508da63214d0d0a9197d8c620e
21120447 <haonhat2729@gmail.com>
2024-06-06 14:42:04 +0700

    fix: browserRouter -> hashRouter

--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import Layout from "../layout/Layout";
 import Home from "../pages/Home";
 import NovelPreview from "../pages/NovelPreview";
@@ -14,85 +14,80 @@ import PrivacyPolicy from "../pages/Information/PrivacyPolicy";
 import AboutLicense from "../pages/Information/AboutLicense";
 import AboutUs from "../pages/Information/AboutUs";
 
-const router = createBrowserRouter(
-  [
-    {
-      element: <ScrollToTop />,
-      children: [
-        {
-          element: <Layout />,
-          children: [
-            {
-              path: "/",
-              element: <Home />,
-            },
-            {
-              path: "/tu-truyen",
-              element: <Bookcase />,
-            },
-            {
-              path: "/truyen/:novelId",
-              children: [
-                {
-                  path: "",
-                  element: <NovelPreview />,
-                },
-                {
-                  element: <CheckNewServer />,
-                  children: [
-                    {
-                      path: ":chapterId",
-                      element: <NovelChapter />,
-                    },
-                  ],
-                },
-              ],
-            },
-            {
-              path: "/tac-gia/:authorId",
-              element: <AuthorPage />,
-            },
-            {
-              path: "/tim-kiem",
-              element: <FilterPage />,
-            },
-            {
-              path: "/thong-tin",
-              children: [
-                {
-                  path: "dieu-khoan-dich-vu",
-                  element: <TermOfService />,
-                },
-                {
-                  path: "chinh-sach-bao-mat",
-                  element: <PrivacyPolicy />,
-                },
-                {
-                  path: "ve-ban-quyen",
-                  element: <AboutLicense />,
-                },
-                {
-                  path: "nhom-phat-trien",
-                  element: <AboutUs />,
-                },
-              ],
-            },
-          ],
-        },
-        {
-          path: "/notfound",
-          element: <Notfound />,
-        },
-        {
-          path: "*",
-          element: <Notfound />,
-        },
-      ],
-    },
-  ],
+const router = createHashRouter([
   {
-    basename: "/",
+    element: <ScrollToTop />,
+    children: [
+      {
+        element: <Layout />,
+        children: [
+          {
+            path: "/",
+            element: <Home />,
+          },
+          {
+            path: "/tu-truyen",
+            element: <Bookcase />,
+          },
+          {
+            path: "/truyen/:novelId",
+            children: [
+              {
+                path: "",
+                element: <NovelPreview />,
+              },
+              {
+                element: <CheckNewServer />,
+                children: [
+                  {
+                    path: ":chapterId",
+                    element: <NovelChapter />,
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            path: "/tac-gia/:authorId",
+            element: <AuthorPage />,
+          },
+          {
+            path: "/tim-kiem",
+            element: <FilterPage />,
+          },
+          {
+            path: "/thong-tin",
+            children: [
+              {
+                path: "dieu-khoan-dich-vu",
+                element: <TermOfService />,
+              },
+              {
+                path: "chinh-sach-bao-mat",
+                element: <PrivacyPolicy />,
+              },
+              {
+                path: "ve-ban-quyen",
+                element: <AboutLicense />,
+              },
+              {
+                path: "nhom-phat-trien",
+                element: <AboutUs />,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        path: "/notfound",
+        element: <Notfound />,
+      },
+      {
+        path: "*",
+        element: <Notfound />,
+      },
+    ],
   },
-);
+]);
 
 export default router;
